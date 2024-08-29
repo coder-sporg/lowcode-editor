f16878731ff49c4fe3581a07c0248b119a7b1ae9
unxxy <xys534393426@163.com>
2024-08-29 19:18:20 +0800

    ✨ feat(store): store持久化

--- src/editor/store/components.tsx
+++ src/editor/store/components.tsx
@@ -1,5 +1,6 @@
 import { CSSProperties } from "react"
-import { create } from "zustand"
+import { create, StateCreator } from "zustand"
+import { persist } from 'zustand/middleware'
 
 export interface Component {
   id: number
@@ -28,102 +29,104 @@ interface Action {
   setCurComponentId: (componentId: number | null) => void
 }
 
-export const useComponentsStore = create<State & Action>(
-  ((set, get) => ({
-    mode: 'edit',
-    components: [
-      {
-        id: 1,
-        name: 'Page',
-        props: {},
-        desc: '页面'
-      }
-    ],
-    curComponentId: null,
-    curComponent: null,
-    setMode: (mode) => set({mode}), // 修改模式
-    setCurComponentId: (componentId) => set(state => {
-      return {
-        curComponentId: componentId,
-        curComponent: getComponentById(state.components, componentId)
-      }
-    }),
-    addComponent: (component, parentId) => set((state) => {
-      // console.log("🚀 ~ addComponent: ~ state:", state) // get() === state
-      if(parentId) {
-        const parentComponent = getComponentById(state.components, parentId)
+const creator: StateCreator<State & Action> = (set, get) => ({
+  mode: 'edit',
+  components: [
+    {
+      id: 1,
+      name: 'Page',
+      props: {},
+      desc: '页面'
+    }
+  ],
+  curComponentId: null,
+  curComponent: null,
+  setMode: (mode) => set({mode}), // 修改模式
+  setCurComponentId: (componentId) => set(state => {
+    return {
+      curComponentId: componentId,
+      curComponent: getComponentById(state.components, componentId)
+    }
+  }),
+  addComponent: (component, parentId) => set((state) => {
+    // console.log("🚀 ~ addComponent: ~ state:", state) // get() === state
+    if(parentId) {
+      const parentComponent = getComponentById(state.components, parentId)
 
-        if(parentComponent) {
-          if(parentComponent.children) {
-            parentComponent.children.push(component)
-          } else {
-            parentComponent.children = [component]
-          }
-        }
-        component.parentId = parentId
-        return {
-          components: [...state.components]
+      if(parentComponent) {
+        if(parentComponent.children) {
+          parentComponent.children.push(component)
+        } else {
+          parentComponent.children = [component]
         }
       }
-      return {
-        components: [...state.components, component]
-      }
-    }),
-    // set 来写，比下面的稍微麻烦
-    // deleteComponent: (componentId) => set((state) => {
-    //   if(!componentId) return { components: state.components }
-
-    //   const component = getComponentById(state.components, componentId)
-    //   if(component?.parentId) {
-    //     const parentComponent = getComponentById(state.components, component.parentId)
-
-    //     if(parentComponent) {
-    //       parentComponent.children = parentComponent.children?.filter(item => item.id !== componentId)
-    //       return { components: state.components }
-    //     }
-    //   }
-    //   return { components: get().components }
-    // }),
-    deleteComponent: (componentId) => {
-      if(!componentId) return
-
-      const component = getComponentById(get().components, componentId)
-
-      if(component?.parentId) {
-        const parentComponent = getComponentById(get().components, component.parentId)
-
-        if(parentComponent) {
-          parentComponent.children = parentComponent.children?.filter(item => item.id !== +componentId)
-
-          set({
-            components: [...get().components]
-          })
-        }
-      }
-    },
-    updateComponentProps: (componentId, props) => set((state) => {
-      const component = getComponentById(state.components, componentId)
-
-      if(component) {
-        component.props = { ...component.props, ...props }
-
-        return { components: [...state.components] }
-      }
-
-      return { components: [...state.components] }
-    }),
-    updateComponentStyles: (componentId, styles, replace) => set(state => {
-      const component = getComponentById(state.components, componentId)
-
-      if(component) {
-        component.styles = replace ? { ...styles } : { ...component.styles, ...styles }
-      }
+      component.parentId = parentId
       return {
         components: [...state.components]
       }
-    })
-  }))
-)
+    }
+    return {
+      components: [...state.components, component]
+    }
+  }),
+  // set 来写，比下面的稍微麻烦
+  // deleteComponent: (componentId) => set((state) => {
+  //   if(!componentId) return { components: state.components }
+
+  //   const component = getComponentById(state.components, componentId)
+  //   if(component?.parentId) {
+  //     const parentComponent = getComponentById(state.components, component.parentId)
+
+  //     if(parentComponent) {
+  //       parentComponent.children = parentComponent.children?.filter(item => item.id !== componentId)
+  //       return { components: state.components }
+  //     }
+  //   }
+  //   return { components: get().components }
+  // }),
+  deleteComponent: (componentId) => {
+    if(!componentId) return
+
+    const component = getComponentById(get().components, componentId)
+
+    if(component?.parentId) {
+      const parentComponent = getComponentById(get().components, component.parentId)
+
+      if(parentComponent) {
+        parentComponent.children = parentComponent.children?.filter(item => item.id !== +componentId)
+
+        set({
+          components: [...get().components]
+        })
+      }
+    }
+  },
+  updateComponentProps: (componentId, props) => set((state) => {
+    const component = getComponentById(state.components, componentId)
+
+    if(component) {
+      component.props = { ...component.props, ...props }
+
+      return { components: [...state.components] }
+    }
+
+    return { components: [...state.components] }
+  }),
+  updateComponentStyles: (componentId, styles, replace) => set(state => {
+    const component = getComponentById(state.components, componentId)
+
+    if(component) {
+      component.styles = replace ? { ...styles } : { ...component.styles, ...styles }
+    }
+    return {
+      components: [...state.components]
+    }
+  })
+})
+
+export const useComponentsStore = create<State & Action>()(persist(creator, {
+  name: 'components'
+}))
 
 export function getComponentById(components: Component[], id: number | null): Component | null {
   if(!id) return null
